16c358cbc0048e64ce219d84b5adecaa87f78f62
Reta <r.range755@gmail.com>
2022-07-28 20:17:51 -0300

    definir modelo de paises

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,5 +4,47 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
-})
+//  [ ] País con las siguientes propiedades:
+    // ID (Código de 3 letras) *
+    id:{
+      type: DataTypes.STRING(3),
+      unique:true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    // Nombre *
+    name:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // Imagen de la bandera *
+    img: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // Continente *
+    region: {
+      type: DataTypes.ENUM('Asia', 'America', 'Antarctica', 'Europe', 'Oceania'),
+      allowNull: false,
+    },
+      // Capital *
+    capital:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+      // Subregión
+    subregion:{
+      type: DataTypes.STRING,
+    },
+    // Área
+    area:{
+      type: DataTypes.INTEGER,
+    },
+      // Población
+    population:{
+      type: DataTypes.INTEGER,
+    },},
+    {
+      timestamps:false,
+    });
 };
