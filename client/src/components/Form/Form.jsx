0bd141e44b24e62d99b1c958ef38d7e3905fc22e
Reta <r.range755@gmail.com>
2022-11-25 19:23:43 -0300

    actualizar formulario

--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -127,95 +127,91 @@ export default function Form(){
 
     return (
         <div className={style.all}>
-            <BotonVolver  />
+            {/* <BotonVolver  /> */}
             <div className={style.containerall}>
-                
+                <h1 className={style.titulo}>Crear actividad</h1>
                 <form className={style.formcontainer} onSubmit={handleSubmit}>
-                    <div className={style.inputitem}>
-                        <label>Nombre:</label>
-                        <input className={style.inputnombre} name="nombre" value={actividad.nombre} onChange={handleChange}/>
-                        {errors.nombre && (<p className={style.errors}>{errors.nombre}</p>)}
-                        {/* --------------- */}
-                        <label className={style.labeldificultad}>Dificultad:</label>
-                        <select 
-                            className={style.selectdificultad}
-                            name="dificultad" 
-                            value={actividad.dificultad}
-                            onChange={handleDificultad}
-                            required
-                        >
-                            <option value={1}>Dificultad</option>
-                            <option value={1}>1</option>
-                            <option value={2}>2</option>
-                            <option value={3}>3</option>
-                            <option value={4}>4</option>
-                            <option value={5}>5</option>
-                        </select>
-                        {errors.dificultad && (<p className={style.errors}>{errors.dificultad}</p>)}
-                        {/* --------------- */}
-                        <label>Duración:</label>
-                        <input className={style.inputduracion} name="duracion" value={actividad.duracion} onChange={handleChange}></input>
-                        {errors.duracion && (<p className={style.errors}>{errors.duracion}</p>)}
-                        {/* --------------- */}
-                        <label>Temporada:</label>
-                        <select 
-                            className={style.selecttemporada}
-                            name="temporada" 
-                            value={actividad.temporada}
-                            onChange={handleTemporadas}
-                            required
-                            
-                        >   
-                            <option value={''}>Temporada</option>
-                            <option value={'Verano'}>Verano</option>
-                            <option value={'Otoño'}>Otoño</option>
-                            <option value={'Invierno'}>Invierno</option>
-                            <option value={'Primavera'}>Primavera</option>
-                        </select>
-                        {errors.temporada && (<p className={style.errors}>{errors.temporada}</p>)}
-                        {/* --------------- */}
-                        <label>Paises:</label>
-                        <select 
-                            className={style.selectpaises}
-                            name="idPais" 
-                            value={actividad.idPais[actividad.idPais.length - 1] ?? ''} 
-                            onChange={handleId} 
-                        >
-                                <option value=''>Countries Select</option>
-                                {allCountries?.map(country => (
-                                    <option key={country.id} value={country.id}>{country.name}</option>
-                                    ))}
-                        </select> 
-                        {errors.idPais && (<p className={style.errors}>{errors.idPais}</p>)}
-
-                        <div>
-                            <h3>Paises en donde agregar la actividad</h3>
+                    <div className={style.conteinerDouble}>
+                        <div className={style.inputitem}>
+                            <label>Nombre:</label>
+                            <input className={style.inputs} name="nombre" value={actividad.nombre} onChange={handleChange}/>
+                            {errors.nombre ? (<p className={style.errors}>{errors.nombre}</p>):<p className={style.errorsFondo}>a</p>}
+                            {/* --------------- */}
+                            <label className={style.labeldificultad}>Dificultad:</label>
+                            <select 
+                                className={style.selectForm}
+                                name="dificultad" 
+                                value={actividad.dificultad}
+                                onChange={handleDificultad}
+                                required
+                            >
+                                <option value={""}>Dificultad</option>
+                                <option value={1}>1</option>
+                                <option value={2}>2</option>
+                                <option value={3}>3</option>
+                                <option value={4}>4</option>
+                                <option value={5}>5</option>
+                            </select>
+                            {errors.dificultad ? (<p className={style.errors}>{errors.dificultad}</p>): <p className={style.errorsFondo}>a</p>}
+                            {/* --------------- */}
+                            <label>Duración:</label>
+                            <input className={style.inputs} name="duracion" value={actividad.duracion} onChange={handleChange}></input>
+                            {errors.duracion ? (<p className={style.errors}>{errors.duracion}</p>) : <p className={style.errorsFondo}>a</p>}
+                            {/* --------------- */}
+                            <label>Temporada:</label>
+                            <select 
+                                className={style.selectForm}
+                                name="temporada" 
+                                value={actividad.temporada}
+                                onChange={handleTemporadas}
+                                required
+                                
+                            >   
+                                <option value={''}>Temporada</option>
+                                <option value={'Verano'}>Verano</option>
+                                <option value={'Otoño'}>Otoño</option>
+                                <option value={'Invierno'}>Invierno</option>
+                                <option value={'Primavera'}>Primavera</option>
+                            </select>
+                            {errors.temporada ? (<p className={style.errors}>{errors.temporada}</p>) : <p className={style.errorsFondo}>a</p>}
+                            {/* --------------- */}
+                        </div>               
+                        <div className={style.contenedorPais}>
+                            <label>Elegir pais/paises:</label>
+                            <select 
+                                className={style.selectForm}
+                                name="idPais" 
+                                value={actividad.idPais[actividad.idPais.length - 1] ?? ''} 
+                                onChange={handleId} 
+                            >
+                                    <option value=''>Countries Select</option>
+                                    {allCountries?.map(country => (
+                                        <option key={country.id} value={country.id}>{country.name}</option>
+                                        ))}
+                            </select> 
+                            {errors.idPais ? (<p className={style.errors}>{errors.idPais}</p>) : <p className={style.errorsFondo}>a</p>}
+                            <h3>Paises en donde se van a agregar la actividad</h3>
                             <table >
-                            <thead>
-                                <tr>
-                                    <th>Paises</th>
-                                    <th>para agregar</th>
-                                </tr>
-                            </thead>
+                            
                             <tbody className={style.paises}>
                                 {actividad.idPais &&
                                 actividad.idPais.map((c) => (
                                     <tr key={c}>
                                     <td>{c}</td>
-                                    <td><button onClick={handleDelete} 
-                                    value={c}>Eliminar</button></td>
+                                    <td><button className={style.eliminar} onClick={handleDelete} 
+                                    value={c}>ELIMINAR</button></td>
                                     </tr>
                                 ))}
                             </tbody>
                             </table>
+                            {/* --------------- */}
+                            {
+                            Object.keys(errors).length === 0 ?
+                            <input className={style.crear} type="submit" value="¡Crear!"/>
+                            :
+                            <input className={style.crearDesactivado} type="submit" value="Complete los campos"/>
+                            }   
                         </div>            
-                        {/* --------------- */}
-                        {
-                        Object.keys(errors).length === 0 ?
-                        <input type="submit" value="Crear"/>
-                        :
-                        <></>
-                        }                   
                     </div>
                 </form>
             </div>
